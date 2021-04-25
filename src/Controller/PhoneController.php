4e6b2bb00aeef67afe2d83bf88b35cb475c7abe8
xibougta <kappler.julien@gmail.com>
2021-04-25 19:49:08 +0200

    Update phone

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -6,9 +6,11 @@ use App\Entity\Phone;
 use App\Repository\PhoneRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * @Route("/api")
@@ -47,5 +49,27 @@ class PhoneController extends AbstractController
         return new Response($data, 200, [
             'Content-Type' => 'application/json'
         ]);
+    }
+
+    /**
+     * @Route("/phones/edit/{id}", name="update_phone", methods={"PUT"})
+     */
+    public function update(Request $request, SerializerInterface $serializer, Phone $phone, EntityManagerInterface $entityManager)
+    {
+        $phoneUpdate = $entityManager->getRepository(Phone::class)->find($phone->getId());
+        $data = json_decode($request->getContent());
+        foreach ($data as $key => $value){
+            if($key && !empty($value)) {
+                $name = ucfirst($key);
+                $setter = 'set'.$name;
+                $phoneUpdate->$setter($value);
+            }
+        }
+        $entityManager->flush();
+        $data = [
+            'status' => 200,
+            'message' => 'Le téléphone a bien été mis à jour'
+        ];
+        return new JsonResponse($data);
     }
 }
